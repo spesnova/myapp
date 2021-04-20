9faede66b99c196ca0e194aed21134dcc5105edd
Seigo Uchida <spesnova@gmail.com>
2021-04-20 13:37:48 +0900

    Use logger for start message

--- myapp/app.js
+++ myapp/app.js
@@ -18,5 +18,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  logger.info(`Example app listening at http://localhost:${port}`);
 })
