05f83c623f9555b46d4b20552f7a1511d2a53f37
Seigo Uchida <spesnova@gmail.com>
2021-04-20 13:28:00 +0900

    Output logs with no colour

--- myapp/app.js
+++ myapp/app.js
@@ -6,12 +6,17 @@ const port = 3000;
 
 logger.level = "info";
 
+log4js.configure({
+  appenders: { out: { type: 'stdout', layout: { type: 'basic' } } },
+  categories: { default: { appenders: ['out'], level: 'info' } }
+});
+
 
 app.get('/', (req, res) => {
-  logger.info("Hello World")
-  res.send('Hello World!')
+  logger.info("Hello World");
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
