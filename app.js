20184a992ebc291c5b95e10f0836200a32486b06
Seigo Uchida <spesnova@gmail.com>
2022-02-09 07:57:00 +0900

    Connect to PostgreSQL

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const app = express();
+
 const log4js = require('log4js');
 const logger = log4js.getLogger();
-const app = express();
+
+const { Pool, Client } = require('pg')
+// pools will use environment variables
+// for connection information
+const pool = new Pool()
+
 const port = 3000;
 
 logger.level = "info";
@@ -13,6 +20,13 @@ log4js.configure({
 
 
 app.get('/', (req, res) => {
+  pool.query('SELECT NOW()', (err, result) => {
+    if (err != null) {
+      logger.error(err)
+    }
+    logger.info("Current time is:", result.rows[0].now)
+    pool.end()
+  })
   logger.info("Hello World");
   res.send('Hello World!');
 })
